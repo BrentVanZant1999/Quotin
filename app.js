d23ca0883ad9f1651ad5a6c3c6443c6238e7f103
BrentVanant1999 <bvancar@yahoo.com>
2018-09-04 14:59:51 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,154 @@
+//app.js
+var express = require('express');
+var app = express();
+var serv = require('http').Server(app);
+
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+
+serv.listen(2000);
+console.log("Server started.");
+
+var SOCKET_LIST = {};
+
+var PlayerObject = function(){
+    var self = {
+        room:-1,
+        lastString:"",
+        points:0,
+        name:"",
+        id:0,
+    }
+    self.updatePoints = function(pointsAdded){
+        self.points += pointsAdded;
+    }
+    return self;
+}
+
+var Player = function(nameVar, idNum){
+    var self = PlayerObject();
+    self.name = nameVar;
+    self.id = idNum;
+    self.room = 0;
+    self.points = 0;
+
+    self.addPoints = function(pointsToAdd) {
+      self.updatePoints(pointsToAdd);
+    }
+    Player.list[idNum] = self;
+    return self;
+}
+//define a list of all players currently online.
+Player.list = {};
+
+Player.onConnect = function(name,socket){
+    var player = Player(name, socket.id);
+    socket.on('keyPress',function(data){
+        if(data.inputId === 'left')
+            player.pressingLeft = data.state;
+        else if(data.inputId === 'right')
+            player.pressingRight = data.state;
+        else if(data.inputId === 'up')
+            player.pressingUp = data.state;
+        else if(data.inputId === 'down')
+            player.pressingDown = data.state;
+    });
+}
+Player.onDisconnect = function(socket){
+    delete Player.list[socket.id];
+}
+Player.update = function(){
+    var pack = [];
+    for(var i in Player.list){
+        var player = Player.list[i];
+        player.update();
+        pack.push({
+            x:player.x,
+            y:player.y,
+            number:player.number
+        });
+    }
+    return pack;
+}
+
+
+var Bullet = function(angle){
+    var self = Entity();
+    self.id = Math.random();
+    self.spdX = Math.cos(angle/180*Math.PI) * 10;
+    self.spdY = Math.sin(angle/180*Math.PI) * 10;
+
+    self.timer = 0;
+    self.toRemove = false;
+    var super_update = self.update;
+    self.update = function(){
+        if(self.timer++ > 100)
+            self.toRemove = true;
+        super_update();
+    }
+    Bullet.list[self.id] = self;
+    return self;
+}
+Bullet.list = {};
+
+Bullet.update = function(){
+    if(Math.random() < 0.1){
+        Bullet(Math.random()*360);
+    }
+
+    var pack = [];
+    for(var i in Bullet.list){
+        var bullet = Bullet.list[i];
+        bullet.update();
+        pack.push({
+            x:bullet.x,
+            y:bullet.y,
+        });
+    }
+    return pack;
+}
+
+var DEBUG = true;
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+    socket.id = Math.random();
+    SOCKET_LIST[socket.id] = socket;
+
+    Player.onConnect(socket);
+
+    socket.on('disconnect',function(){
+        delete SOCKET_LIST[socket.id];
+        Player.onDisconnect(socket);
+    });
+    socket.on('sendMsgToServer',function(data){
+        var playerName = ("" + socket.id).slice(2,7);
+        for(var i in SOCKET_LIST){
+            SOCKET_LIST[i].emit('addToChat',playerName + ': ' + data);
+        }
+    });
+
+    socket.on('evalServer',function(data){
+        if(!DEBUG)
+            return;
+        var res = eval(data);
+        socket.emit('evalAnswer',res);
+    });
+
+
+
+});
+
+setInterval(function(){
+    var pack = {
+        player:Player.update(),
+        bullet:Bullet.update(),
+    }
+
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i];
+        socket.emit('newPositions',pack);
+    }
+},1000/25);
