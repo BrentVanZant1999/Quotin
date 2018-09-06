9dd5b610c0b0e60e72cdcf62ed1dfa873e6743a1
BrentVanant1999 <bvancar@yahoo.com>
2018-09-06 00:52:06 -0500

    lots of html and css styling

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var app = express();
+var express = require("express");
+var app =  express();
 var serv = require('http').Server(app);
-
+const PORT = 2000; //process.env.PORT || 8000;
+app.use(express.static(__dirname + '/client'));
 app.get('/',function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
-app.use('/client',express.static(__dirname + '/client'));
-
-serv.listen(2000);
+app.listen(PORT);
 
 var SOCKET_LIST = {};
 var GENERAL_LIST = {};
